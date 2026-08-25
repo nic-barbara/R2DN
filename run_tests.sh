0d6b28b96c952f0ae7ee0a8e66da9b4c6c3a2288
nic-barbara <nicholas.barbara@epfl.ch>
2026-08-25 20:12:31 +0200

    Added custom VJP to REN backwards pass

--- run_tests.sh
+++ run_tests.sh
@@ -3,3 +3,4 @@
 uv run python test/test_lbdn.py
 uv run python test/test_r2dn.py
 uv run python test/test_ren.py
+uv run python test/test_ren_vjp.py
--- test/benchmark.py
+++ test/benchmark.py
@@ -6,12 +6,18 @@ from robustnn.timing import time_model
 
 jax.config.update("jax_default_matmul_precision", "highest")
 
-# Define contracting REN
-nu = 2
-nx = 4
-nv = 8
-ny = 3
-model = ren.ContractingREN(nu, nx, nv, ny, activation=nn.tanh, init_method="long_memory")
 
-# Time the model
-results = time_model(model, batches=64, horizon=1, n_repeats=200)
+def build_model_and_time(nu, nx, nv, ny, init_method="long_memory"):
+    print(f"\nnx: {nx}, nv: {nv}")
+    model = ren.ContractingREN(
+        nu, nx, nv, ny, activation=nn.tanh, init_method=init_method
+    )
+    return time_model(model, batches=64, horizon=1, n_repeats=200)
+
+
+# Some benchmarking experiments
+nu, ny = 5, 10
+
+build_model_and_time(nu, 16, 32, ny)
+build_model_and_time(nu, 16, 128, ny)
+build_model_and_time(nu, 16, 512, ny)
